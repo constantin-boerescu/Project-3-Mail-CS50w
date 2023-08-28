7d95819faf5adb17941f7fa81545c6a47b7080a4
Boerescu Constantin <costi.boerescu14@gmail.com>
2023-08-28 19:16:04 +0300

    Made inbox and each email page

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  //Send email 
+  document.querySelector('#compose-form').addEventListener('submit', send_mail)
+  
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -14,6 +17,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#each-email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -27,7 +31,107 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#each-email-view').style.display = 'none';
 
   // Show the mailbox name
+  
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails=>{
+    //Print emails
+    console.log(emails);
+    
+    // for each email from the sent box a div is created that stores the data of each email
+    emails.forEach((email) => {
+      // creates a new div element that will hold the email
+      const element = document.createElement('div');
+      // assigns a color for the div if the mail is read or not
+      if (email.read === true){
+        element.classList.add('read');
+        element.classList.remove('not_read');
+      } else{
+        element.classList.add('not_read');
+        element.classList.remove('read');
+
+      }
+      // adds a cass to the element
+      element.classList.add('each_email');
+      // adds the email values to the div
+      element.innerHTML = `<h5>${email.sender}</h5> <p>${email.subject}</p> <p id="time_stamp">${email.timestamp}</p>`
+      // displays a more detailed page of the email
+      element.addEventListener('click', function(){
+        see_each_mail(email.id)
+      });
+      // adds the email into the container
+      document.querySelector('#emails-view').append(element);
+    });
+    
+  })
+  
 }
+
+function send_mail(event){
+  event.preventDefault();
+
+  // Assins the form fields to variables
+  const recipient = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  // Send the data to the backend
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipient,
+      subject: subject,
+      body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    console.log(result);
+    load_mailbox('sent');
+    display_email('sent');
+  })
+
+}
+
+
+function see_each_mail(email_id){
+
+  // gets the email info into json from the API by its ID
+  console.log(email_id)
+  console.log(`/email/${email_id}`)
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    console.log(email)
+    // Show the mailbox and hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#each-email-view').style.display = 'block';
+
+    document.querySelector("#mail_from").innerHTML = email.sender;
+    document.querySelector("#mail_to").innerHTML = email.recipents;
+    document.querySelector("#mail_subject").innerHTML = email.subject;
+    document.querySelector("#mail_timestamp").innerHTML = email.timestamp;
+    document.querySelector("#mail_content").innerHTML = email.body;
+    // TO BE CONTINUED
+    document.querySelector("#archive_btn").addEventListener('click', function(email_id){
+      console.log(`/email/${email_id}`)
+      fetch(`/email/${email_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: true
+        })
+      })
+    });
+  })
+
+
+}
+
+
+
